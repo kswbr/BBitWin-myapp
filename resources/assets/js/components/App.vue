3dcb498fad6c5cfb5d130a3c8f1c8132afe2b2c8
m_kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-03-12 03:45:37 +0900

    eslint導入

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -4,10 +4,10 @@
 
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">ROOT COMPONENT !!</div>
+                    <div class="panel-heading">ROOT COMPONENT !!!</div>
 
                     <div class="panel-body">
-                        I'm an example component!!!!
+                        I'm an example component!!!
                     </div>
                 </div>
             </div>
