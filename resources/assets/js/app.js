fcd624020544e23fb0e0cab65bf712cc72e74b89
m_kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-03-11 15:42:54 +0900

    add passport package and setting

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,23 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('app', require('./components/App.vue'));
+
+console.log('test')
+// Vue.component(
+//     'passport-clients',
+//     require('./components/passport/Clients.vue')
+// );
+//
+// Vue.component(
+//     'passport-authorized-clients',
+//     require('./components/passport/AuthorizedClients.vue')
+// );
+//
+// Vue.component(
+//     'passport-personal-access-tokens',
+//     require('./components/passport/PersonalAccessTokens.vue')
+// );
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">ROOT COMPONENT !!</div>
+
+                    <div class="panel-body">
+                        I'm an example component!!!!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import Axios from 'axios'
+
+    export default {
+        mounted() {
+            console.log('Component mounted!!.')
+            Axios.get('/api/user').then((res) =>  {
+                console.log(res)
+            })
+        }
+    }
+</script>
