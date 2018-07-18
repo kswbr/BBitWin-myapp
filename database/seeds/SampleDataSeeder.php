ebe4437634e15e5752e6d69ff700b9d46fbddc40
m_kashiwabara <m_kashiwabara@bbmedia.co.jp>
2018-07-18 10:30:05 +0900

    seeder少しいじった

--- database/seeds/SampleDataSeeder.php
+++ database/seeds/SampleDataSeeder.php
@@ -20,16 +20,14 @@ class SampleDataSeeder extends Seeder
     {
         $campaign = factory(Campaign::class)->create(["project" => env("PROJECT_NAME", config('app.name'))]);
         $lottery = factory(Lottery::class)->create(['campaign_code' => $campaign->code]);
-        factory(Player::class,10)->create()->each(function($p) use ($lottery) {
-            factory(Entry::class,20)->create([
-                "state" => config("contents.entry.state.lose"),
-                'lottery_code' => $lottery->code,
-                'player_id' => $p->id
-            ]);
+        $dt = new Carbon();
+        factory(Player::class,10)->create()->each(function($p) use ($lottery,$dt) {
+            $dt->addHour(5);
             factory(Entry::class)->create([
                 "state" => config("contents.entry.state.win"),
                 'lottery_code' => $lottery->code,
-                'player_id' => $p->id
+                'player_id' => $p->id,
+                'created_at' => $dt
             ]);
         });
     }
