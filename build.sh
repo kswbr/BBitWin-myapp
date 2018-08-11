9b0a818bfe0c4fb64c92be4d26479c3e13a6755c
bugwabara <m.kashiwabara5963@gmail.com>
2018-08-11 17:35:43 +0900

    mysqlの永続化までできた

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@ GCP_PROJECT=`gcloud config get-value project`
 SHA=`git rev-parse HEAD`
 
 docker build -f php-fpm/Dockerfile -t asia.gcr.io/${GCP_PROJECT}/bbitwin_php-fpm:${SHA} . --no-cache=true
-cd ./nginx && docker build -f Dockerfile -t asia.gcr.io/${GCP_PROJECT}/bbitwin_nginx:${SHA} . --no-cache=true
-
-gcloud docker -- push asia.gcr.io/${GCP_PROJECT}/bbitwin_php-fpm:${SHA}
-gcloud docker -- push asia.gcr.io/${GCP_PROJECT}/bbitwin_nginx:${SHA}
+docker build -f nginx/Dockerfile -t asia.gcr.io/${GCP_PROJECT}/bbitwin_nginx:${SHA} . --no-cache=true
+cd mysql && docker build -f Dockerfile -t asia.gcr.io/${GCP_PROJECT}/bbitwin_mysql:${SHA} . --no-cache=true
