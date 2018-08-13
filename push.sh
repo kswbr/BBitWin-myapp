fdbbb52f0366a455dc3255b7daf8ee149e98fea6
bugwabara <m.kashiwabara5963@gmail.com>
2018-08-14 05:12:10 +0900

    いらなそうだったのでmysqlのbuildコマンド削除

--- push.sh
+++ push.sh
@@ -3,4 +3,3 @@ SHA=`git rev-parse HEAD`
 
 gcloud docker -- push asia.gcr.io/${GCP_PROJECT}/bbitwin_php-fpm:${SHA}
 gcloud docker -- push asia.gcr.io/${GCP_PROJECT}/bbitwin_nginx:${SHA}
-gcloud docker -- push asia.gcr.io/${GCP_PROJECT}/bbitwin_mysql:${SHA}
