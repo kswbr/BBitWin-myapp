d19be39c97ef6268b52647b934e70d057fb329e3
bugwabara <m.kashiwabara5963@gmail.com>
2018-08-13 20:38:48 +0900

    ユーザー作成用のコマンドを作り、代わりにAuthコントローラーのルーティング削除

--- src/production.sh
+++
@@ -1,9 +0,0 @@
-# install composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-chmod -R 777 /var/www/storage
-chmod -R 777 /var/www/bootstrap/cache
-composer install --optimize-autoloader
-php artisan config:cache
-php artisan route:cache
-composer dump-autoload
-php artisan migrate
--- src/routes/web.php
+++ src/routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/admin', 'AdminController@index');
 Route::get('/admin/{any}', 'AdminController@index')->where('any','.*');
 
