58600b7298c1417a41baa15471c21d10ad8f8340
bugwabara <m.kashiwabara5963@gmail.com>
2018-08-14 05:12:10 +0900

    いらなそうだったのでmysqlのbuildコマンド削除

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ SHA=`git rev-parse HEAD`
 
 docker build -f php-fpm/Dockerfile -t asia.gcr.io/${GCP_PROJECT}/bbitwin_php-fpm:${SHA} .
 docker build -f nginx/Dockerfile -t asia.gcr.io/${GCP_PROJECT}/bbitwin_nginx:${SHA} .
-cd mysql && docker build -f Dockerfile -t asia.gcr.io/${GCP_PROJECT}/bbitwin_mysql:${SHA} .
