5e4b919b98fed1a4634cf162a2fa7b979ebc6f8f
bugwabara <m.kashiwabara5963@gmail.com>
2018-08-15 14:51:12 +0900

    自分のアカウント名の編集機能追加

--- src/resources/assets/js/components/UserArea/HeaderBar.vue
+++ src/resources/assets/js/components/UserArea/HeaderBar.vue
@@ -8,7 +8,7 @@
       text-color="#fff"
       active-text-color="#ffd04b">
       <el-menu-item index="1"><span style="font-size:18px;">BbmInstantWin</span></el-menu-item>
-      <el-menu-item class="right" index="100" >{{user.name}}</el-menu-item>
+      <el-menu-item class="right" index="100" ><a @click="editMyAccount">{{user.name}}</a></el-menu-item>
       <el-menu-item class="right" index="99" ><a id="logoutLink" @click="logout">Logout</a></el-menu-item>
     </el-menu>
   </div>
@@ -34,6 +34,9 @@ export default {
       if (window.confirm('ログアウトしますか？')) {
         this.$store.commit(types.LOGGED_OUT)
       }
+    },
+    editMyAccount () {
+      this.$router.push('/admin/userarea/user/' + this.user.id)
     }
   }
 }
---
+++ src/resources/assets/js/components/UserArea/MyAccount/Editor.vue
@@ -0,0 +1,58 @@
+<template>
+  <el-form :model="form" status-icon label-width="180px" >
+    <el-form-item label="ユーザー名">
+      <el-input placeholder="ユーザー名" v-model="form.name"/>
+    </el-form-item>
+    <el-form-item label="メールアドレス">
+      <el-input placeholder="examplemail@dummymail.com" v-model="form.email"/>
+    </el-form-item>
+    <el-form-item >
+      <el-button type="default" @click="() => (this.$router.push('/admin/userarea'))">戻る</el-button>
+      <el-button type="primary" @click="submitForm()">保存</el-button>
+      <el-button type="text" @click="() => (this.$router.push(this.$route.params.id + '/change_password'))">パスワード変更</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+import * as types from '../../../store/mutation-types.js'
+import _ from 'lodash'
+
+export default {
+  name: 'MyAccountEditor',
+  props: {
+    input: Object,
+    save: Function
+  },
+  data () {
+    return {
+      roleList: [],
+      form: {
+        mode:'',
+        name:'',
+        email:'',
+      }
+    }
+  },
+  watch: {
+    input: function (input) {
+      this.form = Object.assign({}, this.input)
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    submitForm () {
+      if (!window.confirm('データを保存しますか？')) {
+        return
+      }
+      this.save(this.form)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
