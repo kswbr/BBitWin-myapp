a4a2af1e6d64cac79fdb3d9231e7d80c8c1bba4d
bugwabara <m.kashiwabara5963@gmail.com>
2018-08-20 05:41:30 +0900

    circleci test

--- src/config/database.php
+++ src/config/database.php
@@ -77,7 +77,19 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
-
+        'circle_test' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circle_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ]
     ],
 
     /*
